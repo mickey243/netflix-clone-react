0d4d6b876e1c8ba34ce237f0fe5850a8aa48560f
mickey243 <mithleshk522@gmail.com>
2021-06-25 00:52:53 +0530

    checking git

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import HomePage from './comonent/HomePage';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome</h1>
+      <HomePage />
     </div>
   );
 }
